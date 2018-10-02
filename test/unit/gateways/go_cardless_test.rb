fbff381753a833a0ab077871a7deadeb74b5c052
JakubKopys <jakubkopys95@gmail.com>
2018-10-02 15:11:08 +0200

    Specs for successful store

--- test/unit/gateways/go_cardless_test.rb
+++ test/unit/gateways/go_cardless_test.rb
@@ -12,6 +12,18 @@ class GoCardlessTest < Test::Unit::TestCase
     }
   end
 
+  def test_successful_store
+    customer_attributes = { 'email' => 'foo@bar.com', 'first_name' => 'John', 'last_name' => 'Doe' }
+    options = { 'billing_address' => { 'country_code' => 'FR' } }
+    bank_account = mock_bank_account
+    stub_gocardless_requests
+
+    response = @gateway.store(customer_attributes, bank_account, options)
+
+    assert_instance_of MultiResponse, response
+    assert_success response
+  end
+
   def test_successful_purchase_with_token
     @gateway.expects(:ssl_request).returns(successful_purchase_response)
 
@@ -32,26 +44,79 @@ class GoCardlessTest < Test::Unit::TestCase
     assert_equal 1000, response.params['payments']['amount']
   end
 
-  # Place raw successful response from gateway here
+  private
+
+  def mock_bank_account
+    mock.tap do |bank_account_mock|
+      bank_account_mock.expects(:first_name).returns('John')
+      bank_account_mock.expects(:last_name).returns('Doe')
+      bank_account_mock.expects(:iban).twice.returns('FR1420041010050500013M02606')
+    end
+  end
+
+  def stub_gocardless_requests
+    @gateway.expects(:ssl_request)
+      .with(:post, 'https://api-sandbox.gocardless.com/customers', anything, anything)
+      .returns(successful_create_customer_response)
+
+    @gateway.expects(:ssl_request)
+      .with(:post, 'https://api-sandbox.gocardless.com/customer_bank_accounts', anything, anything)
+      .returns(successful_create_bank_account_response)
+
+    @gateway.expects(:ssl_request)
+      .with(:post, 'https://api-sandbox.gocardless.com/mandates', anything, anything)
+      .returns(successful_create_mandate_response)
+  end
+
   def successful_purchase_response
-    <<-RESPONSE
-{
-  "payments": {
-    "id": "PM000BW9DTN7Q7",
-    "created_at": "2018-09-18T12:45:18.664Z",
-    "charge_date": "2018-09-21",
-    "amount": 1000,
-    "description": "John Doe - gold: Signup payment",
-    "currency": "EUR",
-    "status": "pending_submission",
-    "amount_refunded": 0,
-    "metadata": {},
-    "links": {
-      "mandate": "MD0004471PDN9N",
-      "creditor": "CR00005PHGZZE7"
-    }
-  }
-}
+    <<~RESPONSE
+      {
+        "payments": {
+          "id": "PM000BW9DTN7Q7",
+          "created_at": "2018-09-18T12:45:18.664Z",
+          "charge_date": "2018-09-21",
+          "amount": 1000,
+          "description": "John Doe - gold: Signup payment",
+          "currency": "EUR",
+          "status": "pending_submission",
+          "amount_refunded": 0,
+          "metadata": {},
+          "links": {
+            "mandate": "MD0004471PDN9N",
+            "creditor": "CR00005PHGZZE7"
+          }
+        }
+      }
+    RESPONSE
+  end
+
+  def successful_create_customer_response
+    <<~RESPONSE
+      {
+        "customers": {
+          "id": "CU0004CKN9T1HZ"
+        }
+      }
+    RESPONSE
+  end
+
+  def successful_create_bank_account_response
+    <<~RESPONSE
+      {
+        "customer_bank_accounts": {
+          "id": "BA00046869V55G"
+        }
+      }
+    RESPONSE
+  end
+
+  def successful_create_mandate_response
+    <<~RESPONSE
+      {
+        "customer_bank_accounts": {
+          "id":"BA0004687N7GD5"
+        }
+      }
     RESPONSE
   end
 end
