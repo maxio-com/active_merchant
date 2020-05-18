0b0a34dda6960f2f5feb54f4c510db7f398ba785
Wendy Smoak <wsmoak@gmail.com>
2020-05-18 16:06:02 -0400

    add test for deleting card only

--- test/remote/gateways/remote_forte_test.rb
+++ test/remote/gateways/remote_forte_test.rb
@@ -219,12 +219,23 @@ class RemoteForteTest < Test::Unit::TestCase
     assert purchase_response.params['transaction_id'].start_with?("trn_")
   end
 
-  def test_successful_store_and_unstore
+  def test_successful_store_and_unstore_of_customer
     assert store_response = @gateway.store(@credit_card, :billing_address => address)
     assert_success store_response
     assert_equal 'Create Successful.', store_response.message
 
     vault_id = store_response.params['customer_token']
+    assert unstore_response = @gateway.unstore(vault_id)
+    assert_success unstore_response
+    assert_equal 'Delete Successful.', unstore_response.message
+  end
+
+  def test_successful_store_of_customer_and_unstore_of_only_paymethod
+    assert store_response = @gateway.store(@credit_card, :billing_address => address)
+    assert_success store_response
+    assert_equal 'Create Successful.', store_response.message
+
+    vault_id = store_response.params['customer_token'] + "|" + store_response.params['default_paymethod_token']
     assert unstore_response = @gateway.unstore(vault_id)
     assert_success unstore_response
     assert_equal 'Delete Successful.', unstore_response.message
