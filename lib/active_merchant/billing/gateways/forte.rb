96e10b34b87406dcf0b58e312fe95ecd8c2db1cf
Marcelo De Polli <marcelo.polli@gmail.com>
2020-05-28 15:44:09 -0300

    Use Hash#delete to filter out params

--- lib/active_merchant/billing/gateways/forte.rb
+++ lib/active_merchant/billing/gateways/forte.rb
@@ -79,8 +79,7 @@ module ActiveMerchant #:nodoc:
       end
 
       def store(payment_method, options = {})
-        customer_token = options[:customer_token]
-        options = options.slice(*(options.keys - [:customer_token]))
+        customer_token = options.delete(:customer_token)
 
         if customer_token.present?
           create_payment_method_for_customer(customer_token, payment_method, options)
