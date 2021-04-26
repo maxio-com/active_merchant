391474faadd98280bda6dafcb4a9ab98dc4cc7b4
Ola Pohorecka Mrożek <aleksandra.pohorecka@chargify.com>
2021-04-26 14:39:58 +0200

    Refactor unit test

--- test/unit/gateways/digital_river_test.rb
+++ test/unit/gateways/digital_river_test.rb
@@ -3,7 +3,7 @@ require 'digital_river'
 
 class DigitalRiverTest < Test::Unit::TestCase
   def setup
-    @gateway = DigitalRiverGateway.new(:token => 'test')
+    @gateway = DigitalRiverGateway.new(token: 'test')
     @backend_gateway = @gateway.instance_variable_get(:@digital_river_gateway)
 
     @store_options = {
@@ -22,13 +22,14 @@ class DigitalRiverTest < Test::Unit::TestCase
   end
 
   def test_successful_store_with_customer_vault_token
-    @gateway
-      .expects(:check_customer_exists)
-      .with("123")
-      .returns(succcessful_customer_exists_response)
-    @gateway
-      .expects(:add_source_to_customer)
-      .with("456", "123")
+    DigitalRiver::ApiClient
+      .expects(:get)
+      .with("/customers/123", anything)
+      .returns(succcessful_customer_response)
+
+    DigitalRiver::ApiClient
+      .expects(:post)
+      .with("/customers/123/sources/456", anything)
       .returns(successful_attach_source_response)
 
     assert response = @gateway.store('456', @store_options.merge(customer_vault_token: '123'))
@@ -36,16 +37,19 @@ class DigitalRiverTest < Test::Unit::TestCase
     assert_instance_of MultiResponse, response
     assert_equal "123", response.primary_response.params["customer_vault_token"]
     assert_equal "456", response.primary_response.params["payment_profile_token"]
+    assert_equal response.message, "OK"
+    assert_equal "123", response.authorization
   end
 
-  def test_successful_store_with_no_customer_vault_token
-    @gateway
-      .expects(:create_customer)
-      .with(@store_options)
-      .returns(succcessful_customer_create_response)
-    @gateway
-      .expects(:add_source_to_customer)
-      .with("456", "123")
+  def test_successful_store_without_customer_vault_token
+    DigitalRiver::ApiClient
+      .expects(:post)
+      .with("/customers", anything)
+      .returns(succcessful_customer_response)
+
+    DigitalRiver::ApiClient
+      .expects(:post)
+      .with("/customers/123/sources/456", anything)
       .returns(successful_attach_source_response)
 
     assert response = @gateway.store('456', @store_options)
@@ -53,57 +57,132 @@ class DigitalRiverTest < Test::Unit::TestCase
     assert_instance_of MultiResponse, response
     assert_equal "123", response.primary_response.params["customer_vault_token"]
     assert_equal "456", response.primary_response.params["payment_profile_token"]
+    assert_equal response.message, "OK"
+    assert_equal "123", response.authorization
   end
 
   def test_unsuccessful_store_when_customer_vault_token_not_exist
-    @gateway
-      .expects(:check_customer_exists)
-      .with("123")
-      .returns(unsuccessful_customer_create_response)
+    DigitalRiver::ApiClient
+      .expects(:get)
+      .with("/customers/123", anything)
+      .returns(unsuccessful_customer_find_response)
 
     assert response = @gateway.store('456', @store_options.merge(customer_vault_token: '123'))
     assert_failure response
     assert_instance_of MultiResponse, response
     assert_equal "Customer '123' not found", response.primary_response.message
     assert_equal false, response.primary_response.params["exists"]
-    assert_equal "123", response.primary_response.authorization
   end
 
-  def succcessful_customer_create_response
-    ActiveMerchant::Billing::Response.new(
-      true,
-      "",
-      {
-        customer_vault_token: "123"
-      },
-      authorization: "123"
+  def test_unsuccessful_store_when_source_already_attached
+    DigitalRiver::ApiClient
+      .expects(:post)
+      .with("/customers", anything)
+      .returns(succcessful_customer_response)
+
+    DigitalRiver::ApiClient
+      .expects(:post)
+      .with("/customers/123/sources/456", anything)
+      .returns(source_already_attached_response)
+
+    assert response = @gateway.store('456', @store_options)
+    assert_failure response
+    assert_equal "Source '456' is attached to another customer. A source cannot be attached to more than one customer. (invalid_parameter)", response.message
+  end
+
+  def succcessful_customer_response
+    stub(
+      success?: true,
+      parsed_response: {
+        id: "123",
+        created_time: "2021-04-26T11:49:58Z",
+        email: "test@example.com",
+        shipping:
+          {
+            address: {
+              line1: "Evergreen Avenue",
+              city: "Bloomfield",
+              postal_code: "43040",
+              state: "OH",
+              country: "US"
+            },
+            name: "John Doe",
+            phone: "1234",
+            organization: "Doe's"
+          },
+        ive_mode: false,
+        enabled: true,
+        request_to_be_forgotten: false,
+        locale: "en_US",
+        type: "individual"
+      }
     )
-  end
-
-  def unsuccessful_customer_create_response
-    ActiveMerchant::Billing::Response.new(
-      false,
-      "Customer '123' not found",
-      {
-        exists: false
-      },
-      authorization: "123"
-    )
-  end
-
-  def succcessful_customer_exists_response
-    ActiveMerchant::Billing::Response.new(true, "Customer found", {exists: true}, authorization: '123')
   end
 
   def successful_attach_source_response
-    ActiveMerchant::Billing::Response.new(
-      true,
-      "",
-      {
-        customer_vault_token: "123",
-        payment_profile_token: "456"
-      },
-      authorization: "123"
+    stub(
+      success?: true,
+      parsed_response: {
+        id: "456",
+        customer_id:  "123",
+        type: "creditCard",
+        reusable: false,
+        owner: {
+          first_name: "William",
+          last_name: "Brown",
+          email: "testing@example.com",
+          address: {
+            line1: "10380 Bren Road West",
+            city: "Minnetonka",
+            state: "MN",
+            country: "US",
+            postal_code: "55343"
+          }
+        },
+        state: "chargeable",
+        created_time: "2021-04-26T11:50:38.983Z",
+        updated_time: "2021-04-26T11:50:38.983Z",
+        flow: "standard",
+        credit_card: {
+          brand: "Visa",
+          expiration_month: 7,
+          expiration_year: 2027,
+          last_four_digits: "1111",
+          payment_identifier: "00700"
+        }
+      }
+    )
+  end
+
+  def unsuccessful_customer_find_response
+    stub(
+      success?: false,
+      parsed_response: {
+        type: "not_found",
+        errors: [
+          {
+            code: "not_found",
+            parameter: "id",
+            message: "Customer '123' not found."
+          }
+        ]
+      }
+    )
+  end
+
+  def source_already_attached_response
+    stub(
+      success?: false,
+      parsed_response: {
+        type: "conflict",
+        errors: [
+          {
+            code: "invalid_parameter",
+            parameter: "sourceId",
+            message: "Source '456' is attached to another customer. A source cannot be attached to more than one customer."
+          }
+        ]
+      }
     )
   end
 end
