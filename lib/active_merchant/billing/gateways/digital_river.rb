f9a4394fc420241d958114a65ff38d034fd7bbb7
Jakub Miziński <jakub.mizinski@chargify.com>
2021-08-17 15:29:23 +0200

    Fix merge

--- lib/active_merchant/billing/gateways/digital_river.rb
+++ lib/active_merchant/billing/gateways/digital_river.rb
@@ -116,7 +116,7 @@ module ActiveMerchant
       def get_charge_capture_id(order_id)
         charges = nil
         sources = nil
-        retry_until(2, "charge not found", 0.5) do
+        retry_until(5, "charge not found", 0.5) do
           order = @digital_river_gateway.order.find(order_id).value!
           charges = order.payment.charges
           sources = order.payment.sources
@@ -125,7 +125,7 @@ module ActiveMerchant
 
         # for now we assume only one charge will be processed at one order
         captures = nil
-        retry_until(2, "capture not found", 0.5) do
+        retry_until(5, "capture not found", 0.5) do
           captures = @digital_river_gateway.charge.find(charges.first.id).value!.captures
           captures&.first.present?
         end
