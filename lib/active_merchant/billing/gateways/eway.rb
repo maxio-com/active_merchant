f23da2c7097faa9c02256cb6f112abb7ab158e84
Wojciech Zakrzewski <asterdrak@gmail.com>
2022-12-08 13:09:47 +0100

    fix

--- lib/active_merchant/billing/gateways/eway.rb
+++ lib/active_merchant/billing/gateways/eway.rb
@@ -61,9 +61,9 @@ module ActiveMerchant #:nodoc:
           gsub(%r((Authorization: Basic )\w+), '\1[FILTERED]').
           gsub(%r((<ewayCardNumber>)\d+(</ewayCardNumber>))i, '\1[FILTERED]\2').
           gsub(%r((<CCNumber>)\d+(</CCNumber>))i, '\1[FILTERED]\2').
-          gsub(%r((<eWAYCustomerID>)\d+(</eWAYCustomerID>))i, '\1[FILTERED]\2').
-          gsub(%r((<Username>)\d+(</Username>))i, '\1[FILTERED]\2').
-          gsub(%r((<Password>)\d+(</Password>))i, '\1[FILTERED]\2').
+          gsub(%r((<eWAYCustomerID>)\w+(</eWAYCustomerID>))i, '\1[FILTERED]\2').
+          gsub(%r((<Username>)\w+(</Username>))i, '\1[FILTERED]\2').
+          gsub(%r((<Password>)\w+(</Password>))i, '\1[FILTERED]\2').
           gsub(%r((<ewayCVN>)\d+(</ewayCVN>))i, '\1[FILTERED]\2')
       end
 
